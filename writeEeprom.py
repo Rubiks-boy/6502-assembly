8f42f4bd1e574ea29893dcfd8d111e973d970277
Adam Walker <adamwalker567123@gmail.com>
2020-07-08 16:32:38 -0700

    Manual hex program

---
+++ writeEeprom.py
@@ -0,0 +1,17 @@
+code = bytearray([
+    # all pins on register b are output
+    0xa9, 0xff,
+    0x8d, 0x02, 0x60,
+
+    # latch output
+    0xa9, 0x69,
+    0x8d, 0x00, 0x60
+])
+rom = code + bytearray([0xea]*(32768 - len(code)))
+
+# First instruction location
+rom[0x7ffc] = 0x00
+rom[0x7ffd] = 0x80
+
+with open("rom.bin", "wb") as f:
+    f.write(rom)
